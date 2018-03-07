cc2f79dfe45556ca533b22a452bd46b3a24317e2
Richard Nespithal <richard@nespithal.com>
2018-03-07 10:42:25 +0100

    remove deprecated -f arg on docker tag

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ docker build --no-cache -t d3v0x/${PACKAGE} .
 
 TAG=$(docker run --rm d3v0x/${PACKAGE} ls /var/db/pkg/net-libs/ | grep ${PACKAGE} | sed -e 's/[a-z]*-//')
 
-docker tag -f d3v0x/${PACKAGE} d3v0x/${PACKAGE}:${TAG}
+docker tag d3v0x/${PACKAGE} d3v0x/${PACKAGE}:${TAG}
 
 echo "Pushing ${PACKAGE}:${TAG}"
 
